327108811909b74d2fb2a38df0d32155085122c6
seedsofchia <ryanchia327@gmail.com>
2025-08-14 05:06:53 +0000

    67

--- robot/Starter.java
+++ robot/Starter.java
@@ -6,39 +6,56 @@ public class Starter implements Directions{
 
   public static void main(String[] args) {
 
+
     World.setVisible(true);// allows us to see the run output
     // the bigger the street, the farther north
     World.setSize(20,20);
-
+    World.setDelay(1);
 
     // The line below creates a Robot that we will refer to as rob
     // Find out what the numbers and direction do!
     // Put rob in a better location for your initials.
-    Robot rob = new Robot(15,2,South,9);
-
+    Robot rob = new Robot(15,2,South,20);
     // Want a second robot?  No prob.  They are cheap :)
     //Robot dude = new Robot(7,5,West,9);
     // examples of commands you can invoke on a Robot
     rob.move();// move one step in the direction it is facing
+   
 
 
     // starting the letter R
     rob.putBeeper();
+    rob.move();
     rob.putBeeper();
+    rob.move();
     rob.putBeeper();
+    rob.move();
+    rob.putBeeper();
+    rob.move();
     rob.putBeeper();
     rob.move();
     rob.putBeeper();
     rob.move();
-    rob.move();
-    rob.move();
     rob.putBeeper();
+    rob.turnLeft();
+    rob.turnLeft();
     rob.move();
     rob.move();
-    rob.putBeeper();
-
+    rob.move();
+    rob.move();
+    rob.move();
+    rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.move();
+    turnRight(rob);
+    rob.move();
     // done with the line, now on the curve
     // rob.turnLeft();
 
   }
+public static void turnRight(Robot rob){
+  for (int i = 0; i < 3; i++)
+  rob.turnLeft();
+}
 }
