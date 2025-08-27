2aa7559703fd12e9c7337aab60b6284e6fa92db9
seedsofchia <ryanchia327@gmail.com>
2025-08-27 22:08:21 +0000

    FINALLLLL COMMITTT

--- math/Math.java
+++ math/Math.java
@@ -71,6 +71,6 @@ class Math {
 
     //Starting at 1000, print on the same line (with a - [hypthen] between each) all of the numbers that end in 0 going down to 0
       for (int z  = 1000; z >= 10; z -= 10)
-      System.out.println(z + "-");
+      System.out.println("-" + z);
   }
 }
