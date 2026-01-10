e990bf66773fc98dff26fe3c9eb930df772fcd49
seedsofchia <ryanchia327@gmail.com>
2026-01-10 05:10:26 +0000

    euler project equation number 1

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,7 +3,13 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        // Your code here.
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if(i % 3 == 0 || i % 5 == 0){
+                sum += i;
+            }
+        }
+        System.out.println("Sum is: " + sum);
 
     }
 }
