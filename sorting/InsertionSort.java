cd3eae22e1f2a82b0731fee4ae6e258e86b3174d
seedsofchia <ryanchia327@gmail.com>
2026-02-03 21:45:04 +0000

    finished selection but need to fix

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -5,4 +5,19 @@ public class InsertionSort implements Sorter {
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
     }
-}
+    public void insertionSort(int[] array) {
+        int n = arr.length;
+        for (int i = 0; i < n -1; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < n; j++){
+                if(a[j] < a[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = a[i];
+            a[i] = a[minIndex];
+            a[minIndex] = temp;
+        }
+        }
+    }
+
