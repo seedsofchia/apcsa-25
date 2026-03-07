6b6521ce1cab2a740590ba3b7b9e87f1b091314f
seedsofchia <ryanchia327@gmail.com>
2026-03-07 03:50:05 +0000

    plotted some points in main

--- polygon/Main.java
+++ polygon/Main.java
@@ -1,9 +1,15 @@
 package polygon;
 
+import java.awt.geom.*;
+
 public class Main {
     public static void main(String [] args)
     {
         IrregularPolygon myPolygon = new IrregularPolygon();
+        myPolygon.add(new Point2D.Double(20, 10));
+        myPolygon.add(new Point2D.Double(70, 20));
+        myPolygon.add(new Point2D.Double(50, 50));
+        myPolygon.add(new Point2D.Double(0, 40));
         myPolygon.draw();
 
         TestSuite.run();
