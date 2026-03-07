4ebab481bd84916f2be2faff2a82d96c776da75a
seedsofchia <ryanchia327@gmail.com>
2026-03-07 03:07:50 +0000

    testing to find the draw suite

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -34,19 +34,21 @@ public class IrregularPolygon {
 
     public double area() {
         // TODO: Calculate the area.
+        
+        Double area = 0.0;
         if (myPolygon.size() < 3) {
             return 0.0;
         }
 
-        double area = 0.0;
         for (int i = 0; i < myPolygon.size(); i++) {
             Point2D.Double current = myPolygon.get(i);
             Point2D.Double next = myPolygon.get((i + 1) % myPolygon.size());
             area += (current.getX() * next.getY());
             area -= (next.getX() * current.getY());
         }
-        
-        return Math.abs(area / 2.0);
+
+        area = Math.abs(area / 2.0);
+        return area;
     }
 
     public void draw()
@@ -55,20 +57,19 @@ public class IrregularPolygon {
         try {
             // TODO: Draw the polygon.
             // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
+            //DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
+            //myDrawingTool.move(50, 50);
             if (myPolygon.size() > 0) {
-                DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
-                
-                Point2D.Double firstPoint = myPolygon.get(0);
-                myDrawingTool.up();
-                myDrawingTool.move(firstPoint.getX(), firstPoint.getY());
-                myDrawingTool.down();
+                DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
+                pen.up();
+                pen.move(myPolygon.get(0).getX(), myPolygon.get(0).getY());
+                pen.down();
 
                 for (int i = 1; i < myPolygon.size(); i++) {
-                    Point2D.Double current = myPolygon.get(i);
-                    myDrawingTool.move(current.getX(), current.getY());
+                    pen.move(myPolygon.get(i).getX(), myPolygon.get(i).getY());
                 }
 
-                myDrawingTool.move(firstPoint.getX(), firstPoint.getY());
+                pen.move(myPolygon.get(0).getX(), myPolygon.get(0).getY());
             }
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
