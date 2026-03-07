ca19b6c018dac736d244b19b6d002e15c69f28af
seedsofchia <ryanchia327@gmail.com>
2026-03-07 02:53:44 +0000

    test suite passed

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -14,17 +14,39 @@ public class IrregularPolygon {
     public void add(Point2D.Double aPoint)
     {
         // TODO: Add a point to the IrregularPolygon.
+        myPolygon.add(aPoint);
     }
 
     public double perimeter() {
         // TODO: Calculate the perimeter.
-        return 3.14;
+        if (myPolygon.size() < 2) {
+            return 0.0;
+        }
+
+        double perimeter = 0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double current = myPolygon.get(i);
+            Point2D.Double next = myPolygon.get((i + 1) % myPolygon.size());
+            perimeter += current.distance(next);
+        }
+        return perimeter;
     }
 
     public double area() {
         // TODO: Calculate the area.
-        Double area = 0.0;
-        return area;
+        if (myPolygon.size() < 3) {
+            return 0.0;
+        }
+
+        double area = 0.0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double current = myPolygon.get(i);
+            Point2D.Double next = myPolygon.get((i + 1) % myPolygon.size());
+            area += (current.getX() * next.getY());
+            area -= (next.getX() * current.getY());
+        }
+        
+        return Math.abs(area / 2.0);
     }
 
     public void draw()
@@ -33,8 +55,21 @@ public class IrregularPolygon {
         try {
             // TODO: Draw the polygon.
             // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
-            //DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
-            //myDrawingTool.move(50, 50);
+            if (myPolygon.size() > 0) {
+                DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
+                
+                Point2D.Double firstPoint = myPolygon.get(0);
+                myDrawingTool.up();
+                myDrawingTool.move(firstPoint.getX(), firstPoint.getY());
+                myDrawingTool.down();
+
+                for (int i = 1; i < myPolygon.size(); i++) {
+                    Point2D.Double current = myPolygon.get(i);
+                    myDrawingTool.move(current.getX(), current.getY());
+                }
+
+                myDrawingTool.move(firstPoint.getX(), firstPoint.getY());
+            }
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
         }
